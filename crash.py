103c0e0330963c69da460b302cbbb53cd9bad0a9
Benjamin5678 <42218528+Benjamin5678@users.noreply.github.com>
2022-08-19 01:10:39 -0400

    Update crash.py

--- crash.py
+++ crash.py
@@ -3,18 +3,17 @@ from time import sleep
 
 #https://youtu.be/F1HA7e3acSI
 def get_multiplier():
+    if(random() < 0.03):
+        return 1
+
     multiplier = 0.01 + 0.99/random()
     return multiplier
 
-def animation(fail, multiplier, bet_cashout):
-    if(fail):
-        print("Crash!!!")
-        return
-    
+def animation(multiplier, bet_cashout):
     animated_cashout = False
     animated_multiplier = 1
     
-    while(animated_multiplier * 1.05 < multiplier):
+    while(animated_multiplier * 1.01 < multiplier):
         animated_multiplier = animated_multiplier * 1.05
         
         if((animated_multiplier > bet_cashout) & (animated_cashout == False)):
@@ -37,14 +36,9 @@ while(cash > 0):
 
     cash = cash - bet_amount
     multiplier = get_multiplier()
-    fail = random() < 0.03
 
-    animation(fail, multiplier, bet_cashout)
-
-    if(fail):
-        print("Sorry, the rocket ship didnt make it off the launch pad. You lost your bet.")
-        continue
-
+    animation(multiplier, bet_cashout)
+    
     if(multiplier < bet_cashout):
         print("Sorry, the rocket ship only made it to", str(multiplier) + ". You lost your bet.")
         continue
